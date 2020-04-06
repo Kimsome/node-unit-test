79570dd5d668ad81c4fabfaa211fb68b9792018a
jinxiaolin <137185065@qq.com>
2020-04-06 20:23:48 +0800

    增加校验内容

--- test/mocha.js
+++ test/mocha.js
@@ -1,5 +1,5 @@
 const { should, expect, assert } = require('chai');
-const { add, mul } = require('../src/math');
+const { add, mul, cover } = require('../src/math');
 
 describe('#math', () => {
     describe('add', () => {
@@ -7,14 +7,28 @@ describe('#math', () => {
             expect(add(2, 3), 5);
         });
 
-        it('should return 5 when 2 + 3', () => {
+        it('should return -1 when 2 + -3', () => {
             expect(add(2, -3), -1);
         });
     });
 
     describe('mul', () => {
-        it('should return 5 when 2 * 3', () => {
-            expect(mul(2, 3), 5);
+        it('should return 6 when 2 * 3', () => {
+            expect(mul(2, 3), 6);
+        });
+    });
+
+    describe('cover', () => {
+        it('should return 1 when cover(2, 1)', () => {
+            expect(cover(2, 1), 1);
+        });
+
+        it('should return 3 when cover(1, 2)', () => {
+            expect(cover(1, 2), 3);
+        });
+
+        it('should return 4 when cover(2, 2)', () => {
+            expect(cover(2, 2), 4);
         });
     });
 });
